5c33e772ce034ef268a9e00ed62b471a6a321f5a
Andela Stevanovic <andjela.s166@gmail.com>
2022-02-20 18:46:22 -0500

    added state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import WeatherContainer from "./WeatherContainer"
+import Header from "./Header"
+import Search from "./Search"
+import About from "./About"
 
 function App() {
+  const [weather, setWeather] = useState({
+    
+      "region": "",
+      "currentConditions": {
+          "dayhour": "",
+          "temp": {
+              "c": 0,
+              "f": 0
+          },
+          "precip": "0%",
+          "humidity": "0%",
+          "wind": {
+              "km": 0,
+              "mile": 0
+          },
+          "iconURL": "",
+          "comment": ""
+      },
+  })
+
+  useEffect(()=> {
+
+  }, [weather])
+
+ 
+
   return (
     <div className="App">
-      
+      <Header />
+      <Search setWeather={setWeather}/>
+      <WeatherContainer weather={weather} />
+      <About />
     </div>
   );
 }
