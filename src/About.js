47c8e016af12f628503fd73d5d11075a9fccc978
Andela Stevanovic <andjela.s166@gmail.com>
2022-02-20 18:45:43 -0500

    added author information

--- src/About.js
+++ src/About.js
@@ -3,7 +3,11 @@ import React from 'react'
 function About() {
   return (
     <div>
-      
+      <h1>Hello World</h1>
+      <p> This application was developed by Angela Stevanovic during the Phase-2 at Flatiron School.
+          </p>
+          <p>You can contact me at <a href="mailto:andjela.s166@gmail.com"> Email</a> 
+          </p>
     </div>
   )
 }
