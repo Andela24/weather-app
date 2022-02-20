897f6139be0eae082b013ba19053af188b46dffd
Andela Stevanovic <andjela.s166@gmail.com>
2022-02-20 18:44:42 -0500

    fetch data

--- src/Search.js
+++ src/Search.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-function Search() {
+function Search({setWeather}) {
+
+    const [searchInput, setSearchInput] = useState("")
+
+// // function handleChange(e) {
+// //     setSearchInput(e.target.value)
+// }
+
+    function getData() {
+        //to avoid CORS error
+        fetch(`https://api.allorigins.win/get?url=${encodeURIComponent(`https://weatherdbi.herokuapp.com/data/weather/${searchInput}`)}`)
+        .then(res=>res.json())
+        .then(data=> {
+            const weatherData=JSON.parse(data.contents)
+            setWeather(weatherData)
+        })
+    }
+    
   return (
     <div>
-      
+        <input type="text" placeholder="Search city" value={searchInput} onChange={(e) => setSearchInput(e.target.value)}></input>
+        <button onClick={getData}>Search</button>
+
     </div>
   )
 }
 
-export default Search
+export default Search;
