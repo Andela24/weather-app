647938af4cd579b44b3e35fff6a72bd2048849df
Andela Stevanovic <andjela.s166@gmail.com>
2022-02-21 16:05:19 -0500

    added img

---
+++ src/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      <img src="https://images.pexels.com/photos/186980/pexels-photo-186980.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="Welcome to Weather App"></img>
+    </div>
+  )
+}
+
+export default Home;
