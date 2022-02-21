125106f6b6c611192884da2f557ee31784ba82ce
Andela Stevanovic <andjela.s166@gmail.com>
2022-02-21 16:08:00 -0500

    updated fetch, changed infos

--- src/DisplayWeather.js
+++ src/DisplayWeather.js
@@ -1,10 +1,37 @@
 import React from "react";
 
+
 function DisplayWeather ({weather}) {
     console.log(weather)
   return (
     <div>
-        <h1>{weather.region} </h1>
+      <table className="container">
+        <tr>
+          <th>City</th>
+          <th>Temp</th>
+          <th>Current Condition</th>
+          <th>Feels Like</th>
+          <th>Humidity</th>
+          <th>Wind</th>
+          <th>Weather</th>
+
+        </tr>
+
+        <tr>
+          <td>{weather?.name}</td>
+          <td>{weather?.main?.temp} F</td>
+          <td>{weather?.weather?.[0].description}</td>
+          <td>{weather?.main?.feels_like}</td>
+          <td>{weather?.main?.humidity}</td>
+          <td>{weather?.wind?.speed}</td>
+          <td><img src={`.src/icons/${weather?.weather?.[0].icon}.png`} alt="pic"/></td>
+         
+
+        </tr>
+
+      </table>
+
+        {/* <h1>{weather.name} </h1>
       <ul>
           <li> Current Conditions: {weather.currentConditions.comment}</li>
           <li>{weather.currentConditions.dayhour} </li>
@@ -21,7 +48,7 @@ function DisplayWeather ({weather}) {
                 <li>Mile: {weather.currentConditions.wind.mile}</li>
             </ul>
           <img src={weather.currentConditions.iconURL} alt="Weather"/>
-      </ul>
+      </ul> */}
     </div>
   );
 }
