c0a8e608a9e451bf5ddfbc579034455416716600
Andela Stevanovic <andjela.s166@gmail.com>
2022-02-21 16:12:01 -0500

    added links

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 
 function Header() {
   return (
     <div>
       <h1>Weather App</h1>
+      <Link to= "/" >Home</Link>
+      <Link to= "/search" >Search</Link>
+      <Link to= "/about" >About</Link>
+     
     </div>
+    
   )
 }
 
