0d8f0a7ed3c26730cf7dafd10c2568261e448e9e
Andela Stevanovic <andjela.s166@gmail.com>
2022-02-23 14:44:48 -0500

    delete request

--- src/App.js
+++ src/App.js
@@ -5,14 +5,29 @@ import Search from './Search';
 import About from './About';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './Home'
-import './App.css'
+import './DisplayWeather.css'
 
 
 function App() {
   const [weather, setWeather] = useState({});
   const [cities, setCities] = useState([])
 
+  useEffect(() => {
 
+  }, [weather])
+
+  function handleDelete(id) {
+    // console.log('delete city', city)
+    fetch(`http://localhost:3000/favorites/${id}`, {
+      method: "DELETE"
+  })    
+    const updatedFavorites= cities.filter((city) => city.id !== id)
+    setCities(updatedFavorites)
+  }
+
+
+
+  
 
   return (
     <Router>
@@ -24,7 +39,7 @@ function App() {
           </Route>
             <Route path="/search">
               <Search setWeather={setWeather} cities={cities} setCities={setCities} />
-              <WeatherContainer weather={weather} cities={cities} setCities={setCities} />
+              <WeatherContainer weather={weather} cities={cities} setCities={setCities} handleDelete={handleDelete} />
             </Route>
             <Route path="/about">
               <About />
