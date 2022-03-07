508c40231e3ea587251a517cecfd9759cb65d732
Andela Stevanovic <andjela.s166@gmail.com>
2022-03-07 11:49:15 -0500

    delete unused code

--- src/LikeBtn.js
+++ src/LikeBtn.js
@@ -1,23 +1,18 @@
 import React, {useState} from 'react'
 import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
 
 function LikeBtn() {
     const [count, setCount] = useState(0)
-    // const [formData, setFormData]= useState("")
+  
    
 
     function handleCount() {
         setCount((count) => count + 1)
       }
 
-    // function handleForm(e) {
-    //     setFormData(e.target.value)
-        
-    // }
+  
   return (
     <div>
-        {/* <TextField size="small" type="text" placeholder="enter number" value={formData} onChange={handleForm} /> */}
       <Button variant="outlined" onClick={handleCount}>Like {count} </Button>
     </div>
   )
